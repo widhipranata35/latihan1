9f6b07b8110f216f86fb1bb91d246c2183ae712c
widhipranata35 <widhipranata35@gmail.com>
2020-04-09 11:57:31 +0700

    perintahtugas3

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+
+// variabel dan tipe data
+// variabel = digunakan untuk menyimpan data
+// aturan variabel 'tidak boleh diawali dengan angka, tapi boleh mengandung angka'
+
+$nama = "widhi";
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,9 +15,7 @@
 	</title>
 </head>
 <body>
+	<h1>hallo, selamat datang <?php echo $widhi;?></h1>
 
-<?php
-echo "<h1>selamat datang, widhi</h1>";
-?>
 </body>
 </html>

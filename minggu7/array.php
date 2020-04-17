218e9a7f060a66d17e16d77fedd5be9016f0660f
widhipranata35 <widhipranata35@gmail.com>
2020-04-17 20:26:32 +0700

    task1

---
+++ minggu7/array.php
@@ -0,0 +1,20 @@
+<?php
+//cara lama
+$hari = array("senin", "slasa", "Rabu",);
+//panggil array
+//var_dump($hari);
+//echo $hari[0];
+//print_r($hari);
+//echo "<br>";
+
+//cara baru
+
+//$bulan = ["januari","februari","maret"];
+//print_r($bulan);
+print_r($hari);
+$hari[] = "kamis";
+echo "<br>";
+print_r($hari);
+
+
+?>
